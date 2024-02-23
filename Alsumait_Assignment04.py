5fa90a8208ae1679da5c8c89468c01be6a57b506
alsumait.b <alsumait.b@northeastern.edu>
2024-02-23 18:24:35 -0500

    Defining all methods

--- Alsumait_Assignment04.py
+++ Alsumait_Assignment04.py
@@ -1,2 +1,16 @@
-# Assignment 04 To do list
+# Assignment 04 To Do list
+
+def main():
+    tasks = input("Enter your tasks: ").split()
+    
+    
+    
+    
+def add_tasks(tasks):
+        
+def view_tasks(tasks):
+        
+def mark_tasks(tasks):
+    
+def delete_tasks(tasks):
 
