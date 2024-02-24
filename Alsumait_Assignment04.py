7743566f711f59281639befed9e2f0442656bf8e
alsumait.b <alsumait.b@northeastern.edu>
2024-02-24 14:00:37 -0500

    Changed main function, added while loop with menu

--- Alsumait_Assignment04.py
+++ Alsumait_Assignment04.py
@@ -2,31 +2,45 @@
 
 def main():
     tasks = []
-    add_tasks(tasks)
-    view_tasks(tasks)
-   
-    
+    while True:
+        print("1. Add New Task")
+        print("2. View All Tasks")
+        print("3. Mark Task as Completed")
+        print("4. Delete Task")
+        print("0. Quit Program")
+        
+        user_selection = int(input("Please enter a option from the menu above: "))
+        
+        if user_selection == 0:
+            break
+        elif user_selection == 1:
+            pass
+        elif user_selection == 2:
+            pass
+        elif user_selection == 3:
+            pass
+        elif user_selection == 4:
+            pass
     
 def add_tasks(tasks):
     question = input("Would you like to add tasks? ")
     if question == "yes":
         adding = input("Enter your tasks: ")
-    tasks.append(adding)
+        tasks.append(adding)
     return list(tasks)
         
 def view_tasks(tasks):
     question = input("Would you like to view your tasks? ")
     if question == "yes":
-        for task in tasks:
-            print(f"Your tasks are: {task}")
+        print(tasks)
+        #for task in tasks:
+         #   print(f"Your tasks are: {task}")
     return tasks
 
 
             
 
-    print("Task deleted.")
     
-    return list(tasks)
 
 
 
