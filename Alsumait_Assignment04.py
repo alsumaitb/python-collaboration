95cad480719c9ec2c9b291bcf567c768c7568e14
alsumait.b <alsumait.b@northeastern.edu>
2024-02-23 19:14:50 -0500

    Done adding and viewing tasks

--- Alsumait_Assignment04.py
+++ Alsumait_Assignment04.py
@@ -5,14 +5,28 @@ def main():
     add_tasks(tasks)
     view_tasks(tasks)
     
+    
 def add_tasks(tasks):
-    adding = input("Enter your tasks: ")
+    question = input("Would you like to add tasks? ")
+    if question == "yes":
+        adding = input("Enter your tasks: ")
     tasks.append(adding)
     return list(tasks)
         
 def view_tasks(tasks):
-    for task in tasks:
-        print(f"Your tasks are: {task}")
+    question = input("Would you like to view your tasks? ")
+    if question == "yes":
+        for task in tasks:
+            print(f"Your tasks are: {task}")
+            
+
+        
+        
+        
+     
+
+
+  
         
 
         
